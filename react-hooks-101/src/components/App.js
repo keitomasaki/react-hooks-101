5262a84d839e5722c4432aaec9599739d238944f
masaki <keitomasaki@icloud.com>
2021-02-27 19:38:27 +0900

    save state width localstrage

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import EventForm from "./EventForm";
@@ -7,13 +7,22 @@ import AppContext from "../contexts/AppContext";
 import reducer from "../reducers";
 import OperationLogs from "./OperationLogs";
 
+const APP_KEY = "appWidthRedux";
+
 const App = () => {
-  const initialState = {
-    events: [],
-    operationLogs: [],
-  };
+  const appState = localStorage.getItem(APP_KEY);
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
 
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
